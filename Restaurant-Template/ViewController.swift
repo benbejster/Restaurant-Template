41d34a6a463f95a1f8b2e4feadac25e1515dcd1d
Ben Bejster <benbejster@outlook.com>
2018-07-23 18:48:34 -0500

    Change color to blue

--- Restaurant-Template/ViewController.swift
+++ Restaurant-Template/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = .blue
     }
 
 
